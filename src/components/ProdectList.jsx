53963c850c7791dfb61d2c52f5bba5d7c14d3336
mohamedfaridelsaid <mohammed_eliraqi@yahoo.com>
2020-01-26 12:36:00 +0200

    style products

--- src/components/ProdectList.jsx
+++ src/components/ProdectList.jsx
@@ -1,14 +1,9 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./title";
-import { storeProducts } from "../data";
 import { ProductConsumer } from "../context";
 class ProductList extends Component {
-  state = {
-    products: storeProducts
-  };
   render() {
-    console.log(this.state.products);
     return (
       <React.Fragment>
         <div className="py-5">
@@ -17,7 +12,9 @@ class ProductList extends Component {
             <div className="row">
               <ProductConsumer>
                 {value => {
-                  console.log(value);
+                  return value.products.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  });
                 }}
               </ProductConsumer>
             </div>
